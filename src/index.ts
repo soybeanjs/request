7c8d909cf69ca174dd7596df08fcdb20da06db0a
Soybean <soybeanjs@outlook.com>
2026-02-03 13:49:13 +0800

    fix(index): export BACKEND_ERROR_FLAG from constant

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { createRequest, createFlatRequest } from './core';
-
+export { BACKEND_ERROR_FLAG } from './constant';
 export { parseContentDisposition, downloadFile } from './shared';
 
 export type * from './types';
