65985cf6f4c28c12f0799f0ab4f0b784df48a8bf
Soybean <soybeanjs@outlook.com>
2026-06-07 11:34:27 +0800

    chore(deps): update deps

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -4,7 +4,9 @@ import pkg from './package.json' with { type: 'json' };
 export default defineConfig({
   entry: ['src/index.ts'],
   platform: 'neutral',
-  external: Object.keys(pkg.dependencies),
+  deps: {
+    neverBundle: Object.keys(pkg.dependencies)
+  },
   clean: true,
   dts: true,
   shims: true,
